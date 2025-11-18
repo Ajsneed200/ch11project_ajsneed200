fe11ea8a3012b30a5d7fcea0f78f24a0560b3e88
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-11-17 23:47:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE HTML>
-    <html>
+<html>
 
-    <head>
+<head>
+		<link rel="stylesheet" type="text/css" href="styles/main.css" />
+</head>
+<body>
+	<h1>Building a Dynamic Form with PHP</h1>
+	<p>Andrew Sneed</p>
+	<p>11/7/2025</p>
+	<img src= "screenshot_1.png">
+	<img src= "screenshot_2.png">
+</body>
 
-    </head>
-    <body>
-<h1>Building a Dynamic Form with PHP</h1>
-<p>Andrew Sneed</p>
-<p>11/7/2025</p>
-<img src= "screenshot.png">
-    </body>
-	
-    </html>
+</html>
