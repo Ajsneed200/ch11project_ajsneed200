ce7d53115c22cc1a679061777d0acba5ea91a7cb
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-11-17 23:39:11 -0500

    Add basic HTML structure with dynamic form title

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+    <html>
+
+    <head>
+
+    </head>
+    <body>
+<h1>Building a Dynamic Form with PHP</h1>
+<p>Andrew Sneed</p>
+<p>11/7/2025</p>
+<img src= "screenshot.png">
+    </body>
+	
+    </html>
